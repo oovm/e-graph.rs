76b86bb8ea54ae95e903541d0fa9696dd2c79409
SasakiSaki <192608617@qq.com>
2024-01-23 16:41:26 +0800

    Initial commit

---
+++ projects/game-extension/src/lib.rs
@@ -0,0 +1,24 @@
+#![deny(missing_debug_implementations, missing_copy_implementations)]
+#![warn(missing_docs, rustdoc::missing_crate_level_docs)]
+#![doc = include_str!("../readme.md")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+#![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+
+mod errors;
+mod player;
+
+pub use crate::errors::{GameError, GameErrorKind, Result};
+
+use godot::prelude::*;
+
+#[derive(Copy, Clone, Debug)]
+pub struct GodotExtension {}
+
+#[gdextension]
+unsafe impl ExtensionLibrary for GodotExtension {}
+
+#[test]
+fn ready() {
+    GodotExtension {};
+    println!("it works!")
+}
